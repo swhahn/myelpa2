33426cca89dfcadca6620488df2c0d9a6e240a78
Stefan-W. Hahn <stefan.hahn@s-hahn.de>
2024-08-25 08:11:57 +0200

    Added package example

---
+++ .cask/31.0/elpa/github-elpa-20231201.804/github-elpa-pkg.el
@@ -0,0 +1,14 @@
+(define-package "github-elpa" "20231201.804" "Build and publish ELPA repositories with GitHub Pages"
+  '((package-build "1.0")
+    (commander "0.7.0")
+    (git "0.1.1"))
+  :commit "c818883d9dc8d34eaee03691574e0408f18db28a" :authors
+  '(("10sr" . "8slashes+el@gmail.com"))
+  :maintainers
+  '(("10sr" . "8slashes+el@gmail.com"))
+  :maintainer
+  '("10sr" . "8slashes+el@gmail.com")
+  :url "https://github.com/10sr/github-elpa")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
